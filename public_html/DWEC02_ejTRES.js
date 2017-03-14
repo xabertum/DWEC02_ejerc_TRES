06d9da6e7577b38d4744dacb112c5f30195dfee9
xabertum <mohatar@gmail.com>
2017-03-14 15:37:45 +0100

    minor commit

--- public_html/DWEC02_ejTRES.js
+++ public_html/DWEC02_ejTRES.js
@@ -5,10 +5,21 @@
  */
 
 
+
+function pedirOperacion () {
+    
+    var operacion = prompt ("Introduce la operación\n '+' '-' '*' '/' '%'")
+    
+    return operacion;
+    
+}
+
+
+
 function pedirNumero() {
 
-var n1 = prompt("Introduce el numero 1: ");
-var n2 = prompt("introduce el numero 2: ");
+    var n1 = prompt("Introduce el numero 1: ");
+    var n2 = prompt("introduce el numero 2: ");
 
 }
 
@@ -21,4 +32,18 @@ function resta(n1, n2) {
     return n1 - n2;
 }
 
+function multiplicacion(n1, n2) {
+    return n1 * n2;
+}
+
+function division(n1, n2) {
+    return n1 / n2;
+}
+
+function modulo(n1, n2) {
+    return n1 % n2;
+}
+
+
+pedirOperacion();
 pedirNumero();
